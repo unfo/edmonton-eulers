f421b45aaf223ddfa2af98ce45a410a77b360301
Jan Wikholm <jan.wikholm@conmio.com>
2011-04-04 13:25:58 +0300

    Added description of the problem and more functions to get Lychrels

--- Euler55.hs
+++ Euler55.hs
@@ -1,4 +1,38 @@
 module Main where
+{-
+If we take 47, reverse and add, 47 + 74 = 121, which is palindromic.
+
+Not all numbers produce palindromes so quickly. For example,
+
+349 + 943 = 1292,
+1292 + 2921 = 4213
+4213 + 3124 = 7337
+
+That is, 349 took three iterations to arrive at a palindrome.
+
+Although no one has proved it yet, it is thought that some numbers, like 196, never produce a palindrome. A number that never forms a palindrome through the reverse and add process is called a Lychrel number. Due to the theoretical nature of these numbers, and for the purpose of this problem, we shall assume that a number is Lychrel until proven otherwise. In addition you are given that for every number below ten-thousand, it will either (i) become a palindrome in less than fifty iterations, or, (ii) no one, with all the computing power that exists, has managed so far to map it to a palindrome. In fact, 10677 is the first number to be shown to require over fifty iterations before producing a palindrome: 4668731596684224866951378664 (53 iterations, 28-digits).
+
+Surprisingly, there are palindromic numbers that are themselves Lychrel numbers; the first example is 4994.
+
+How many Lychrel numbers are there below ten-thousand?
+-}
 
 revInt :: Integer -> Integer
 revInt i = read (reverse (show i))::Integer
+
+reverseAndAdd :: Integer -> Integer
+reverseAndAdd i = i + (revInt i)
+
+recursionLimit = 50
+
+
+isPalindrome :: Integer -> Bool
+isPalindrome i = i == (revInt i)
+
+isLychrel :: Integer -> Bool
+isLychrel i = findPalindromeUpToLimit (reverseAndAdd i) 0 recursionLimit
+  where 
+    findPalindromeUpToLimit number iteration limit 
+      | isPalindrome number = True
+      | iteration < limit   = findPalindromeUpToLimit (reverseAndAdd number) (iteration + 1) recursionLimit
+      | otherwise           = False 
